5b2bcba37864b6f926940225f8d1b3436ab2ff02
Jonathan Aguirre <yonathanaguirre@live.com>
2016-03-18 09:48:27 -0300

    Commit #4
    Agrego input de que tipo de lenguaje de programacion.

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 </head>
 <body>
  <div class="container">
-      <div class="freshdesignweb-top"><h1><a href="http://www.freshdesignweb.com">PRIMER PROYECTO UTN</a></h1></div><div class="clr"></div>
+      <div class="freshdesignweb-top"><h1><a href="https://github.com/skydarkness47/form-registro--1">PRIMER PROYECTO UTN</a></h1></div><div class="clr"></div>
 			<header>
 				<h1><span></span><strong> FORMULARIO DE REGISTRO JONATHAN AGUIRRE</strong></h1>
             </header>       
@@ -24,20 +24,42 @@
     			 
     			<p class="contact"><label for="email">CORREO ELECTRONICO</label></p> 
     			<input id="email" name="email" placeholder="ejemplo@domain.com" required="" type="email"> 
-                
-                <p class="contact"><label for="username">PAGINA WEB</label></p> 
-    			<input id="username" name="username" placeholder="PAGINA WEB" required="" tabindex="2" type="url"> 
-    			 
+
+    		
+                 <p class="contact"><label for="username"></label></p> 
+          <input id="username" name="username" placeholder="PAGINA WEB" required="" tabindex="2" type="url"> 
+        
                 <p class="contact"><label for="password">INGRESE UNA CONTRASEÑA</label></p> 
     			<input type="password" id="password" name="password" required=""> 
                 <p class="contact"><label for="repassword">REINGRESE LA CONTRASEÑA</label></p> 
-    			<input type="password" id="repassword" name="repassword" required=""> 
+    			<input type="password" id="repassword" name="repassword" required="">
+
+          <script type="text/javascript">
+            
+              function comparar(){
+                if(document.form.password.value == document.form.repassword.value)
+                {
+                  document.form.submit();
+
+                }
+
+                
+              }
+              var x1 =document.form.password.value;
+             
+              var x2= document.form.repassword.value;
+
+            
+
+
+          </script>
+         
         
-               <fieldset>
+                         <fieldset>
                  <label>CUMPLEAÑOS</label>
                   <label class="month"> 
                   <select class="select-style" name="BirthMonth">
-                  <option value="">MES</option>
+                  <option value=""> MES</option>
                   <option  value="01">ENERO</option>
                   <option value="02">FEBRERO</option>
                   <option value="03" >MARZO</option>
@@ -57,33 +79,31 @@
               </fieldset>
               <br>
                  <fieldset>
-                 <label>SEXO</label>
+                 <label>SELECCIONE UN SEXO SEXO</label>
                   <label class="month"> 
                   <select class="select-style" name="BirthMonth">
                   <option value="">SEXO</option>
                   <option  value="01">HOMBRE</option>
                   <option value="02">MUJER</option>
-              
+ 
                   </label>
                  </select>   
               </fieldset>
+          <fieldset>
+                <br> <input type="radio" name = "lenguajep" value="PROGRAMACION 3" checked="">PROGRAMACION 3<br>
+                 <br> <input type="radio" name = "lenguajep" value="PROGRAMACION 3" checked="">LABORATORIO 3<br>
+
+               </fieldset>
+             
               <br>
              <br>
-            <p class="contact"><label for="phone">Mobile phone</label></p> 
-            <input id="phone" name="phone" placeholder="phone number" required="" type="text"> <br>
-            <input class="buttom" name="submit" id="submit" tabindex="5" value="Sign me up!" type="submit"> 	 
+            <p class="contact"><label for="phone">TELEFONO</label></p> 
+            <input id="phone" name="phone" placeholder="NUMERO DE TELEFONO" required="" type="text"> <br>
+            <input class="buttom" name="submit" id="submit" tabindex="5" value="REGISTRO" type="submit" onclick="comparar(x1,x2)"> 	 
    </form> 
   </div>       
 </div>
-            <!-- freshdesignweb top bar -->
-            <div class="freshdesignweb-buttom">
-                <a href="http://www.freshdesignweb.com" target="_blank">Home</a>
-                <span class="right">
-                    <a href="http://www.freshdesignweb.com/beautiful-registration-form-with-html5-and-css3.html">
-                        Back to the freshdesignweb Article
-                    </a>
-                </span>
-                <div class="clr"></div>
-            </div><!--/ freshdesignweb top bar -->
+         
+            
 </body>
 </html>
